647fb88b8996909d4b71d71f0cbf1f4bd59db981
MikeSphx <kim.michae@husky.neu.edu>
2017-09-14 03:55:34 -0400

    Added code for creating UDP sockets as well as sending and receiving data between them

--- ChatClient.py
+++ ChatClient.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import socket
 
 def main():
     parser = argparse.ArgumentParser()
@@ -18,6 +19,28 @@ def main():
     user = arguments.user
     sip = arguments.server_ip
     sp = arguments.server_port
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error:
+        print 'Failed to create socket'
+        sys.exit()
+
+    while True:
+        msg = raw_input('+> ')
+
+        try:
+            s.sendto(msg, (sip, port))
+
+            d = s.recvfrom(1024)
+            reply = d[0]
+            addr = d[1]
+         
+            print 'Server reply : ' + reply
+     
+        except socket.error, msg:
+            print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit()
 
 if __name__ == "__main__":
     main()
--- ChatServer.py
+++ ChatServer.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import socket
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,10 +9,41 @@ def main():
     arguments = parser.parse_args()
 
     # Test
-    print "SP: " + str(arguments.server_port)
+    #print "SP: " + str(arguments.server_port)
 
     # Saving agruments passed from command line
     sp = arguments.server_port
+    # Initailizing host parameter for the socket
+    host = ''
+
+    # Create UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Attempt to associate the socket to a network interface and port
+    try:
+        s.bind((host, sp))
+    except:
+        print 'Failed to bind socket'
+        sys.exit(-1)
+
+    print 'Socket bind successful'
+
+    while True:
+        data, addr = s.recvfrom(1024)
+
+        if not data:
+            break
+
+        reply = 'OK...' + data
+
+        s.sendto(reply, addr)
+        print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+
+        #print 'Connected with ' + address[0] + ':' + str(address[1])
+        #start_new_thread( , conn)
+
+    s.close()
+
 
 if __name__ == "__main__":
     main()
